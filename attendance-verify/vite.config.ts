6aa25d8d725dbf3625147a99371ac7ba43318d65
Anamay Narkar <anamay.narkar.102@gmail.com>
2025-09-02 13:01:20 +0530

    minor changes in backend

--- attendance-verify/vite.config.ts
+++ attendance-verify/vite.config.ts
@@ -5,4 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api/*': {
+        target: process.env.VITE_SERVER_URL || 'http://localhost:3000',
+      }   
+    }
+  }
 })
